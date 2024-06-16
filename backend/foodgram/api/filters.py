00516ee619a6cd8d5670a2f12fa1ff501bb07dc7
rodion <rpadakov@yandex.ru>
2024-06-16 15:47:31 +0800

    avatar ednpoint

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -24,7 +24,7 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(favoriterecipe__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
