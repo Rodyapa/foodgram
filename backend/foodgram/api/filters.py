b9ccb1123f6a15eaa5fe3177ccd714e85a5b7519
rodion <rpadakov@yandex.ru>
2024-06-18 18:22:39 +0800

    after first review

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -29,5 +29,5 @@ class RecipeFilter(filters.FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
-            return queryset.filter(shopingcart__user=self.request.user)
+            return queryset.filter(in_cart__user=self.request.user)
         return queryset
