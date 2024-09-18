986ad0a13aede8e42433f064246ed4e860040f1e
rodion <rpadakov@yandex.ru>
2024-09-18 20:04:11 +0800

    debugging main workflow

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -26,7 +26,7 @@ def create_ingredients_list(request):
 
 def make_pdf_file_of_ingredients(final_list):
     # Check for the correct font path in production
-    font_path = os.path.join('../static/fonts/Lato-Regular.ttf')
+    font_path = '../static/fonts/Lato-Regular.ttf'
 
     pdfmetrics.registerFont(TTFont('Lato-Regular', font_path))
 
